6fcf118c9be4552d79fccbabf2d6c49cd4cc87e3
DylanMorison <dylan@g.morison.org>
2021-05-10 20:22:30 -0700

    Arr and Slice

---
+++ arrays_and_slices/playground.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// create an array called "firstArray" that has length 3
+	// and holds integers
+	firstArray := [3]int{1, 2, 3}
+	fmt.Printf("firstArray: %v", firstArray)
+}
