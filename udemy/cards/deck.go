fae6d0c7a14b341499183a9ae5c6a5baa49d2748
Dylan Morison <dylan@g.morison.org>
2021-06-16 16:58:30 -0700

    cards

--- udemy/cards/deck.go
+++ udemy/cards/deck.go
@@ -6,6 +6,24 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Cubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"}
+
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// any var of type deck
+// has access to the print method
+// this is the point of a receiver method
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
