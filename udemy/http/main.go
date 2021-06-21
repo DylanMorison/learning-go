bfac37242c0194271f20a2802494b885e5d39204
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2021-06-21 14:15:27 -0700

    mac work

--- udemy/http/main.go
+++ udemy/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -13,5 +14,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	io.Copy(os.Stdout, resp.Body)
 }
+
+	// fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// n, e := resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// fmt.Println(n)
+	// fmt.Println(e)
