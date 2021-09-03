701d87f4e6b33220694004e58f65e412c4ef395e
DylanMorison <dylan@g.morison.org>
2021-09-03 16:40:59 -0700

    refresh

--- refresher/hello.go
+++ refresher/hello.go
@@ -3,5 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+	x := 7
+	inc(&x)
+	fmt.Println(x)
+} 
+
+func inc(x *int) {
+	*x++
 }
+
+// func sum(x int, y int) int {
+// 	return x + y
+// }
+
+// //! map
+// func main() {
+// 	vertices := make(map[string]int)
+
+// 	vertices["triangle"] = 2
+// 	vertices["square"] = 4
+// 	vertices["dodeas"] = 12
+
+// 	fmt.Println(vertices["triangle"])
+// }
+
+// //! map
+// func main() {
+// 	vertices := make(map[string]int)
+
+// 	vertices["triangle"] = 2
+// 	vertices["square"] = 4
+// 	vertices["dodeas"] = 12
+
+// 	fmt.Println(vertices["triangle"])
+// }
+//! slice
+// func main() {
+// 	a := []int{}
+// 	a = append(a, 13)
+// 	fmt.Println(a)
+// }
+//! Arr
+// func main() {
+// 	a := [6]int{}
+// 	a = append(a, 13)
+// 	fmt.Println(a)
+// }
+ 
